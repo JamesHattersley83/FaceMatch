d5648d51b7fb280cbfffa49de03199866a489d34
James Hattersley <jameshattersley83@gmail.com>
2019-05-21 22:53:27 +0100

    added bootstrap and created navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        facematch
+      </a>
+      <button className="btn btn-outline-light my-2 my-sm-0" type="button">
+        Sign Out
+      </button>
+    </nav>
+  );
+};
+
+export default Navigation;
