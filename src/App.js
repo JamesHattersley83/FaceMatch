00ed348bae4391b163e5f056dade1f1f4a8beed9
James Hattersley <jameshattersley83@gmail.com>
2019-05-22 23:17:54 +0100

    added event handlers to input and button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Rank />
-      <ImageLinkForm />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
 
-      {/* {
-      <FaceRecognition />} */}
-    </div>
-  );
+  onInputChange = event => {
+    console.log(event.target.value);
+  };
+
+  onSubmit = () => {
+    console.log("Click");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onSubmit={this.onSubmit}
+        />
+
+        {/* {
+        <FaceRecognition />} */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,22 +1,24 @@
 import React from "react";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div className="container">
       <h5 className="text-center mt-5">
         {"This App will detect faces in your photos. Give it a try!"}
       </h5>
-      <div class="input-group mt-3">
+      <div className="input-group mt-3">
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           placeholder="Enter photo URL..."
+          onChange={onInputChange}
         />
-        <div class="input-group-append">
+        <div className="input-group-append">
           <button
-            class="btn btn-outline-secondary"
+            className="btn btn-outline-secondary"
             type="button"
             id="button-addon2"
+            onClick={onSubmit}
           >
             Detect
           </button>
