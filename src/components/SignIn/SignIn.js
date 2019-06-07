9bc30e9aee11b723bf83f3dea9b8d6e7a4ab05e5
James Hattersley <jameshattersley83@gmail.com>
2019-06-07 22:59:45 +0100

    connected frontend signin to backend api

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,73 +1,109 @@
 import React from "react";
 import "./SignIn.css";
 
-const SignIn = ({ onRouteChange }) => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-          <div className="card card-signin my-5">
-            <div className="card-body">
-              <h5 className="card-title text-center">Sign In</h5>
-              <form className="form-signin">
-                <div className="form-label-group">
-                  <input
-                    type="email"
-                    id="inputEmail"
-                    className="form-control"
-                    placeholder="Email address"
-                    required
-                    autoFocus
-                  />
-                  <label htmlFor="inputEmail">Email address</label>
-                </div>
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: "",
+      signInPassword: ""
+    };
+  }
+  onEmailChange = event => {
+    this.setState({ signInEmail: event.target.value });
+  };
+  onPasswordChange = event => {
+    this.setState({ signInPassword: event.target.value });
+  };
 
-                <div className="form-label-group">
-                  <input
-                    type="password"
-                    id="inputPassword"
-                    className="form-control"
-                    placeholder="Password"
-                    required
-                  />
-                  <label htmlFor="inputPassword">Password</label>
-                </div>
+  onSubmitSignIn = () => {
+    fetch("http://localhost:3000/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data === "success") {
+          this.props.onRouteChange("home");
+        }
+      });
+  };
 
-                <div className="custom-control custom-checkbox mb-3">
-                  <input
-                    type="checkbox"
-                    className="custom-control-input"
-                    id="customCheck1"
-                  />
-                  <label
-                    className="custom-control-label"
-                    htmlFor="customCheck1"
+  render() {
+    const { onRouteChange } = this.props;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card card-signin my-5">
+              <div className="card-body">
+                <h5 className="card-title text-center">Sign In</h5>
+                <div className="form-signin">
+                  <div className="form-label-group">
+                    <input
+                      type="email"
+                      id="inputEmail"
+                      className="form-control"
+                      placeholder="Email address"
+                      required
+                      autoFocus
+                      onChange={this.onEmailChange}
+                    />
+                    <label htmlFor="inputEmail">Email address</label>
+                  </div>
+
+                  <div className="form-label-group">
+                    <input
+                      type="password"
+                      id="inputPassword"
+                      className="form-control"
+                      placeholder="Password"
+                      required
+                      onChange={this.onPasswordChange}
+                    />
+                    <label htmlFor="inputPassword">Password</label>
+                  </div>
+
+                  <div className="custom-control custom-checkbox mb-3">
+                    <input
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="customCheck1"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customCheck1"
+                    >
+                      Remember password
+                    </label>
+                  </div>
+                  <button
+                    className="btn btn-lg btn-primary btn-block text-uppercase"
+                    type="submit"
+                    onClick={this.onSubmitSignIn}
                   >
-                    Remember password
-                  </label>
+                    Sign in
+                  </button>
+                  <hr className="my-4" />
+                  <button
+                    className="btn btn-lg btn-google btn-block text-uppercase"
+                    type="submit"
+                    onClick={() => onRouteChange("register")}
+                  >
+                    <i className="fab fa-google mr-2" /> Register
+                  </button>
                 </div>
-                <button
-                  className="btn btn-lg btn-primary btn-block text-uppercase"
-                  type="submit"
-                  onClick={() => onRouteChange("home")}
-                >
-                  Sign in
-                </button>
-                <hr className="my-4" />
-                <button
-                  className="btn btn-lg btn-google btn-block text-uppercase"
-                  type="submit"
-                  onClick={() => onRouteChange("register")}
-                >
-                  <i className="fab fa-google mr-2" /> Register
-                </button>
-              </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SignIn;
